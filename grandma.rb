97fd067911d6ac4720ab2a9b8e4acc05b7ae4741
Madeline Celis <madelinebcelis@gmail.com>
2018-07-20 16:41:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -17,6 +17,7 @@ def speak_to_grandma(string)
     puts 'NO, NOT SINCE 1938!'
   elsif string == 'I LOVE YOU GRANDMA!'
     puts 'I LOVE YOU TOO PUMPKIN!'
-  else puts 'HUH?! SPEAK UP, SONNY!'
+  else
+    puts 'HUH?! SPEAK UP, SONNY!'
   end
 end
